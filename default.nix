d45f98aef0bbc2cbdf11c207efb340ec3eb372ff
Geraldo Nascimento <geraldogabriel@gmail.com>
2024-08-15 15:48:07 -0300

    default.nix: changes for Heurasync Sonic

--- default.nix
+++ default.nix
@@ -1,23 +1,24 @@
-let
-    pkgs = (import (builtins.fetchTarball {
-        url = "https://github.com/NixOS/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.zip";
-        sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
-    }) { });
-    stdenv = pkgs.stdenv;
+let pkgs = (import <nixpkgs> {}); in let unstable-pkgs = (import <nixpkgs-unstable> {});
+
 in pkgs.mkShell rec {
     name = "interview";
     shellHook = ''
         source .bashrc
+        export MALLOC_TRIM_THRESHOLD_=131072
     '';
     buildInputs = (with pkgs; [
         bashInteractive
         (pkgs.python3.buildEnv.override {
             ignoreCollisions = true;
-            extraLibs = with pkgs.python3.pkgs; [
+            extraLibs = [
                 # package list: https://search.nixos.org/packages
                 # be parsimonious with 3rd party dependencies; better to show off your own code than someone else's
-                ipython
-                nose
+                pkgs.python3.pkgs.lxml
+                pkgs.python3.pkgs.charset-normalizer
+                pkgs.python3.pkgs.h2
+                pkgs.python3.pkgs.aiodns
+                unstable-pkgs.python3.pkgs.onecache
+                unstable-pkgs.python3.pkgs.aiosonic
             ];
         })
     ]);
